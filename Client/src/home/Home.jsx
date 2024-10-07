bb33d87bc63af103b1facc36ca65281b05c8f400
shruti018kanani <155936554+shruti018kanani@users.noreply.github.com>
2024-10-07 11:35:28 +0530

    Promotion Banner

--- Client/src/home/Home.jsx
+++ Client/src/home/Home.jsx
@@ -12,12 +12,3 @@ const Home = () => {
 }
 
 export default Home
-
-{/* <div className=''>
-      <div className=' h-screen'>
-        Home
-      </div>
-      <div className=' h-screen bg-red-600'>
-
-      </div>
-    </div> */}
